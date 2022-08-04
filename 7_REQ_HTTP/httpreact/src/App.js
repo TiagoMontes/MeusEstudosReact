3aab08b8ba54b66db9d908bdece105152c9b4a9c
TiagoMontes <tiagosantos6303@gmail.com>
2022-08-04 16:00:18 -0500

    Fetch de dados

---
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+
+import { useState, useEffect } from "react"
+
+const url = "http://localhost:3000/products"
+
+function App() {
+  const [products, setProducts] = useState([])
+
+  // 1 - resgatando dados
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+    
+    fetchData()
+  }, []);
+
+  // 2 - Add product
+
+  return (
+    <div className="App">
+      <h1>Lista de produtos</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>
+            {product.name} - R${product.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
