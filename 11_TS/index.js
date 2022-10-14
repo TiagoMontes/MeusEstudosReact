7fd0a56bb437e93aac8ec81e30ffb91f3f7e57e0
Sarah Barbosa <sarahbarbosa@MacBook-Air-de-Sarah.local>
2022-10-13 21:00:24 -0500

    testando

--- 11_TS/index.js
+++ 11_TS/index.js
@@ -2,7 +2,7 @@ var firstName = "Tiago";
 var anotherName = 1;
 var x = true;
 function greeting(name) {
-    console.log("Olá " + name);
+    console.log("Olá " + name, ", Testando");
 }
 greeting(firstName);
 // greeting(anotherName);
