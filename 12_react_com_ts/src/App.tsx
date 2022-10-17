8bf367ad16f7bf9a810b42086e06fbc3e50e7c90
Tiago Montes <tiagosantos6303@gmail.com>
2022-10-16 21:48:06 -0500

    testando novamente

--- 12_react_com_ts/src/App.tsx
+++ 12_react_com_ts/src/App.tsx
@@ -70,7 +70,7 @@ function App() {
       {myText && <p>Have text in the variable</p>}
       {mySecondText && <p>Have text in the variable</p>}
       {fixedText && <p>Fixed test</p>}
-      <p>Just testing</p>
+      <p>Just testing.</p>
     </div>
   );
 }
