4fc7a397e999b7850c5f30e83b30e06f29222792
Tiago Montes <tiagosantos6303@gmail.com>
2022-10-17 15:37:35 -0500

    configuracoes iniciais

---
+++ 13_todo_react_ts/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//components
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <h1>Conteúdo...</h1>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
