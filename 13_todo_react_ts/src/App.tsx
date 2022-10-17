b67ebc4f0de6bd708d72fc8a490c968ac90b86db
Tiago Montes <tiagosantos6303@gmail.com>
2022-10-17 18:27:04 -0500

    Formulario e adicao de tasks

--- 13_todo_react_ts/src/App.tsx
+++ 13_todo_react_ts/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 //components
 import { Footer } from './components/Footer';
@@ -9,14 +9,24 @@ import { TaskList } from './components/TaskList';
 //CSS
 import styles from './App.module.css';
 
+//Interface
+import { ITask } from './interfaces/Task';
+
 function App() {
+
+  const [taskList, setTaskList] = useState<ITask[]>([]);
+
   return (
     <div className="App">
       <Header />
       <main className={styles.main}>
         <div>
           <h2>O que você vai fazer?</h2>
-          <TaskForm btnText='Criar tarefa'/>
+          <TaskForm
+            btnText='Criar tarefa'
+            taskList={taskList}
+            setTaskList={setTaskList}
+          />
         </div>
         <div>
           <h2>Suas tarefas:</h2>
