7f3e821ccf609417f4e6d80e41a4af431024c5dc
Sarah Barbosa <sarahbarbosa@MacBook-Air-de-Sarah.local>
2022-10-16 21:42:33 -0500

    its working

--- 12_react_com_ts/src/App.tsx
+++ 12_react_com_ts/src/App.tsx
@@ -70,7 +70,7 @@ function App() {
       {myText && <p>Have text in the variable</p>}
       {mySecondText && <p>Have text in the variable</p>}
       {fixedText && <p>Fixed test</p>}
-      <p>Just testing a git</p>
+      <p>Just testing</p>
     </div>
   );
 }
