b640d6a79d89d8a4fa4b2510c9f233c6b18b7d16
Tiago Montes <tiagosantos6303@gmail.com>
2022-10-17 19:24:48 -0500

    html lista e finalizando

--- 13_todo_react_ts/src/components/TaskList.tsx
+++ 13_todo_react_ts/src/components/TaskList.tsx
@@ -15,8 +15,15 @@ const TaskList = ({taskList}: Props) => {
     <>
       {taskList.length > 0 ? (
         taskList.map((task) => (
-          <div key={task.id}>
-            <h3>{task.title}</h3>
+          <div key={task.id} className={styles.task}>
+            <div className={styles.details}>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div className={styles.actions}>
+              <i className='bi bi-pencil'></i>
+              <i className='bi bi-trash'></i>
+            </div>
           </div>
         ))
       ) : (
